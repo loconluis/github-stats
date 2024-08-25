164351627de3950e2ef8d091705bd9b7dd344ca3
Luis Locon <luis.locon@gmail.com>
2024-08-25 15:52:06 -0600

    Adding some seed data to test

---
+++ src/utils/seed.ts
@@ -0,0 +1,46 @@
+import { Seed } from '../interfaces';
+
+export const seed: Seed = {
+  repo: [
+    {
+      id: 1,
+      name: 'R1',
+      url: 'http/hisahsjha.net',
+      stargazers_count: 7,
+      language: 'Typescript',
+      forks_count: 2,
+    },
+    {
+      id: 2,
+      name: 'R2',
+      url: 'http/hisahsjha.net',
+      stargazers_count: 6,
+      language: 'Typescript',
+      forks_count: 1,
+    },
+    {
+      id: 3,
+      name: 'R3',
+      url: 'http/hisahsjha.net',
+      stargazers_count: 2,
+      language: 'Typescript',
+      forks_count: 22,
+    },
+    {
+      id: 4,
+      name: 'R4',
+      url: 'http/hisahsjha.net',
+      stargazers_count: 7,
+      language: 'Java',
+      forks_count: 2,
+    },
+    {
+      id: 5,
+      name: 'R5',
+      url: 'http/hisahsjha.net',
+      stargazers_count: 7,
+      language: 'Java',
+      forks_count: 2,
+    },
+  ],
+};
